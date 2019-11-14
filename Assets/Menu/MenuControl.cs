042fdabe10911233e00c0bbf53689a92f7938f27
VladislavFl <43980608+VladislavFl@users.noreply.github.com>
2019-11-14 11:32:18 +0300

    proj
    interfaces

---
+++ Assets/Menu/MenuControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Menu1");
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Test1()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Test2()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void Test3()
+    {
+        SceneManager.LoadScene("Game3");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Menu/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public float timer;
+    public bool ispause;
+    public bool guipause;
+
+    void Update()
+    {
+        Time.timeScale = timer;
+        if (Input.GetKeyDown(KeyCode.Escape) && ispause == false)
+        {
+            ispause = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && ispause == true)
+        {
+            ispause = false;
+        }
+        if (ispause == true)
+        {
+            timer = 1f;
+            guipause = true;
+
+        }
+        else if (ispause == false)
+        {
+            timer = 1f;
+            guipause = false;
+
+        }
+    }
+    public void OnGUI()
+    {
+        if (guipause == true)
+        {
+            Cursor.visible = true;
+            if (GUI.Button(new Rect((float)(Screen.width / 2) - 75f, (float)(Screen.height / 2) - 150f, 150f, 45f), "Продолжить"))
+            {
+                ispause = false;
+                timer = 1;
+                Cursor.visible = true;
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2) - 75f, (float)(Screen.height / 2) - 100f , 150f, 45f), "В Меню"))
+            {
+                ispause = false;
+                timer = 1;
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
